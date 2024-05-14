cbb273f538d75fe88c4a3f322e30015ef1dc7f93
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-05-14 17:15:30 -0300

    feat: add sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.scss";
+import "../styles/specs.scss";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
