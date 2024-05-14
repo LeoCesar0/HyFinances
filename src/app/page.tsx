6d7a25cc3ed33025c33e050d41014446997a1ee6
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-05-14 18:07:39 -0300

    feat: add user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={"/dashboard"}>Go to Dashboard</Link>
+    <main className="">
+      <header className="bg-indigo-300 p-4 flex items-center justify-between">
+        <div>
+          <h1>Brand new Header</h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link href={"/dashboard"}>Go to Dashboard</Link>
+          <UserButton />
+        </div>
+      </header>
     </main>
   );
 }
