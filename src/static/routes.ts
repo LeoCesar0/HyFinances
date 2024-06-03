5f9ce6d05f68bbf9a0aeddaebdc2d1b4364784c8
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-03 16:12:42 -0300

    feat: add dashboard routes

---
+++ src/static/routes.ts
@@ -0,0 +1,22 @@
+export const DASHBOARD_ROUTES = [
+  {
+    href: "/dashboard",
+    label: "Overview",
+  },
+  {
+    href: "/dashboard/transactions",
+    label: "Transactions",
+  },
+  {
+    href: "/dashboard/accounts",
+    label: "Accounts",
+  },
+  {
+    href: "/dashboard/categories",
+    label: "Categories",
+  },
+  {
+    href: "/dashboard/settings",
+    label: "Settings",
+  },
+];
