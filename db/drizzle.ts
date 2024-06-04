7fefbb5c4530aded83252e7b7482bcca1717a488
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-04 12:04:58 -0300

    chore: drizzle migrations

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,5 +1,8 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
 export const DRIZZLE_SQL = neon(process.env.DATABASE_URL!);
-export const DRIZZLE_DB = drizzle(DRIZZLE_SQL);
+export const DRIZZLE_DB = drizzle(DRIZZLE_SQL, {
+  schema,
+});
--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,9 +1,9 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
 
 
-
-export const accounts = pgTable("accounts", {
+export const accountsSchema = pgTable("accounts", {
     id: text("id").primaryKey(),
     name: text("name").notNull(),
     userId: text("userId").notNull(),
+    plaidId: text("plaidId"),
 })
