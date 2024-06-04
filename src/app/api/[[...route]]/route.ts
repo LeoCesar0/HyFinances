503516e3f64f125b8e2e62e4cc154c9935de05f7
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-04 12:09:49 -0300

    feat: add accounts route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -4,45 +4,13 @@ import { handle } from "hono/vercel";
 import { z } from "zod";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
+import accounts from "./accounts";
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("/hello", (c) => {
-    return c.json({
-      message: "Hello Next.js!",
-    });
-  })
-  .get(
-    "/hello/:id",
-    zValidator(
-      "param",
-      z.object({
-        id: z.string().min(3),
-      })
-    ),
-    (c) => {
-      const { id } = c.req.valid("param");
-      return c.json({
-        message: "Hello ID --> " + id,
-      });
-    }
-  );
-
-app.get("/user", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      message: "Not Authenticated",
-    });
-  }
-
-  return c.json({
-    ...auth,
-  });
-});
+app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
