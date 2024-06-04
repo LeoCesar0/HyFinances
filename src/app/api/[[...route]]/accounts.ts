16072415acbf49e9797b3d85b1041be2c9141621
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-04 12:19:17 -0300

    chore: hono types

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
+import { DRIZZLE_DB } from "../../../../db/drizzle";
+import { accountsSchema } from "../../../../db/schema";
 
-const app = new Hono();
+const app = new Hono().get("/", async (ctx) => {
+  const data = await DRIZZLE_DB.select({
+    id: accountsSchema.id,
+    name: accountsSchema.name,
+  }).from(accountsSchema);
 
-app.get("/", (ctx) => {
-  return ctx.json({ accounts: [] });
+  return ctx.json({ data: data });
 });
 
 export default app;
